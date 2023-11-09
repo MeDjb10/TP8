bf8a08cab044d79e4317c3f788c6cbc30218279c
MeDjb10 <131858818+MeDjb10@users.noreply.github.com>
2023-11-09 06:29:19 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProduitsComponent } from './application/produits/produits.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProduitsComponent,
+    ModelsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
